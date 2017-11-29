9717cba0394b17bfc90afd8f9a36873dabc6b358
Paul Ellis <pellis@qti.qualcomm.com>
2017-11-29 11:21:03 -0800

    Figuring out CPU use

--- main.js
+++ main.js
@@ -2,51 +2,63 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function() {
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
+    var startingCPU = Game.cpu.getUsed();
+    
+
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-    
+
 
     var harvesters = _.filter(Game.creeps,
                               (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps,
                              (creep) => creep.memory.role == 'upgrader');
     var testIfCanSpawn =
-        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE],
                                          'creep' + Game.time,
                                          { dryRun: true });
 
-    console.log('Harvesters: ' + harvesters.length);       
-    console.log('Upgraders: ' + upgraders.length);
+    //    console.log('Harvesters: ' + harvesters.length);
+    //    console.log('Upgraders: ' + upgraders.length);
 
     if (harvesters.length < 2) {
         if (testIfCanSpawn == OK) {
             var newName = 'harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE],
                 newName, {memory: {role: 'harvester'}});
         }
     }
-    
+
     if (upgraders.length < 2) {
         if (testIfCanSpawn == OK) {
             var newName = 'upgrader' + Game.time;
             console.log('Spawning new upgrader: ' + newName);
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE],
                 newName, {memory: {role: 'upgrader'}});
         }
     }
 
+    var managementCPU = Game.cpu.getUsed() - startingCPU;
+
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
     }
+
+    var creepCPU = Game.cpu.getUsed() - managementCPU;
+
+    console.log('bucket: ' + Game.cpu.bucket);
+    console.log('limit: ' + Game.cpu.limit);
+    console.log('managementCPU: ' + managementCPU);
+    console.log('creepCPU: ' + creepCPU);
 }
