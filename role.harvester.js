b104435c0529c842dbf3c2610768b49df9851f44
Paul Ellis <Paul@home>
2017-12-03 22:24:48 -0800

    Fix bugs, add harvester talking, remove useless CPU loop

--- role.harvester.js
+++ role.harvester.js
@@ -1,7 +1,7 @@
 var roleHarvester = {
-   
-    /** Harvesters are state machines. 
-    
+
+    /** Harvesters are state machines.
+
        State: commuting
 
        Description: Harvester is moving to a source to harvest or to a
@@ -23,66 +23,81 @@ var roleHarvester = {
        Transition to: creep.transfer(, RESOURCE_ENERGY)
        Transition from: creep.carry.energy == 0
     */
-    
-    /** @param {Creep} creep **/    
-    run: function(creep) {
-	if (creep.memory.state == "new") {
-	    var destinations = creep.room.find(FIND_SOURCES);
-	    if (sources.length > 0) {
-		creep.memory.state = "commuting";
-		creep.memory.destination = destinations[0].id;
-	    }
-	}
-	
-	if (creep.memory.state == "commuting") {
-	    var destination = Game.getObjectById(creep.memory.destination);
 
-	    if (destination.pos.isNearTo(creep.pos)) {
-		if (creep.carry.energy == 0) {
-		    creep.memory.state = "harvesting";
-		}
-		else {
-		    creep.memory.state = "delivering";
-		}
-	    }
-	    else {
-		creep.moveTo(destination,
-			     {visualizePathStyle: {stroke: '#ffaa00'}});
-	    }
-	}
-	else if (creep.memory.state == "harvesting") {
-	    if (creep.carry.energy == creep.carryCapacity) {
-		var destinations = creep.room.find(FIND_STRUCTURES, {
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if (creep.memory.state != "commuting" &&
+	    creep.memory.state != "delivering" &&
+	    creep.memory.state != "harvesting") {
+            var destinations = creep.room.find(FIND_SOURCES);
+            if (destinations.length > 0) {
+                creep.memory.state = "commuting";
+                creep.memory.destination = destinations[0].id;
+		creep.say('commuting');
+            }
+        }
+
+        if (creep.memory.state == "commuting") {
+            var destination = Game.getObjectById(creep.memory.destination);
+
+            if (destination.pos.isNearTo(creep.pos)) {
+                if (creep.carry.energy == 0) {
+                    creep.memory.state = "harvesting";
+		    creep.say('harvesting');
+                }
+                else {
+                    creep.memory.state = "delivering";
+		    creep.say('delivering');
+                }
+            }
+            else {
+                creep.moveTo(destination,
+                             {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else if (creep.memory.state == "harvesting") {
+            if (creep.carry.energy == creep.carryCapacity) {
+                var destinations = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-			return ((structure.structureType ==
-				 STRUCTURE_EXTENSION ||
-				 structure.structureType == STRUCTURE_SPAWN) &&
-				structure.energy < structure.energyCapacity);
+                        return ((structure.structureType ==
+                                 STRUCTURE_EXTENSION ||
+                                 structure.structureType == STRUCTURE_SPAWN) &&
+                                structure.energy < structure.energyCapacity);
                     }
-		});
-		if(targets.length > 0) {
-		    creep.memory.state = "commuting";
-		    creep.memory.destination = destinations[0].id;
+                });
+                if (destinations.length > 0) {
+                    creep.memory.state = "commuting";
+                    creep.memory.destination = destinations[0].id;
+		    creep.say('commute');
+                }
+            }
+            else {
+                var destination = Game.getObjectById(creep.memory.destination);
+                creep.harvest(destination);
+            }
+        }
+        else if (creep.memory.state == "delivering") {
+            if (creep.carry.energy == 0) {
+                var destinations = creep.room.find(FIND_SOURCES);
+                if (destinations.length > 0) {
+                    creep.memory.state = "commuting";
+                    creep.memory.destination = destinations[0].id;
+		    creep.say('commute');
+                }
+            }
+            else {
+                var destination = Game.getObjectById(creep.memory.destination);
+                if (OK != creep.transfer(destination, RESOURCE_ENERGY)) {
+		    /* Fix transition bug. */
+                    var destinations = creep.room.find(FIND_SOURCES);
+                    if (destinations.length > 0) {
+			creep.memory.state = "commuting";
+			creep.memory.destination = destinations[0].id;
+			creep.say('commute');
+                    }
 		}
-	    }
-	    else {
-		var destination = Game.getObjectById(creep.memory.destination);
-		creep.harvest(destination);
-	    }
-	}
-	else if (creep.memory.state == "delivering") {
-	    if (creep.carry.energy == 0) {
-		var destinations = creep.room.find(FIND_SOURCES);
-		if (sources.length > 0) {
-		    creep.memory.state = "commuting";
-		    creep.memory.destination = destinations[0].id;
-		}
-	    }
-	    else {
-		var destination = Game.getObjectById(creep.memory.destination);
-		creep.transfer(destination, RESOURCE_ENERGY);
-	    }
-	}
+            }
+        }
     }
 };
 
